b780c7e5a1646a1d02e7ed2663c7e783e22738aa
Maciej Góralczyk <goralczykmaciek@gmail.com>
2024-10-29 17:34:18 +0100

    Created project

---
+++ src/main/java/com/maciejgoralczyk/ESportWebApp/ESportWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.maciejgoralczyk.ESportWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESportWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESportWebAppApplication.class, args);
+	}
+
+}
