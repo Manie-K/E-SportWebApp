f667dbd05afc9b3a84859c89de0a7d0cbc273e22
Maciej Góralczyk <goralczykmaciek@gmail.com>
2024-12-27 20:58:19 +0100

    Added backend docker files

--- ESportWebApp_Gateway/src/main/java/com/maciejgoralczyk/esportapp/ESportWebApp/config/GatewayConfiguration.java
+++ ESportWebApp_Gateway/src/main/java/com/maciejgoralczyk/esportapp/ESportWebApp/config/GatewayConfiguration.java
@@ -1,5 +1,7 @@
 package com.maciejgoralczyk.esportapp.ESportWebApp.config;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,18 +10,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfiguration{
     @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder,
+                                      @Value("${ESPORT_GATEWAY_HOST}") String gatewayUrl,
+                                      @Value("${ESPORT_PLAYER_SERVICE_URL}") String playersUrl,
+                                      @Value("${ESPORT_ORGANIZATION_SERVICE_URL}") String organizationsUrl
+    ) {
         return builder.routes()
                 .route("player-service", r -> r
-                        .host("localhost:8081")
+                        .host(gatewayUrl)
                         .and()
                         .path("/api/players/**", "/api/events/**")
-                        .uri("http://localhost:8083"))
+                        .uri(playersUrl))
                 .route("organization-service", r -> r
-                        .host("localhost:8081")
+                        .host(gatewayUrl)
                         .and()
                         .path("/api/organizations/**")
-                        .uri("http://localhost:8082"))
+                        .uri(organizationsUrl))
                 .build();
     }
 }
