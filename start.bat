ef8e847d3faa7ed23dfd3fb294c04d09a6119656
Maciej Góralczyk <goralczykmaciek@gmail.com>
2024-12-29 19:43:17 +0100

    Connected frontend to gateway

--- start.bat
+++ start.bat
@@ -8,4 +8,7 @@ cd ..
 cd ESportWebApp_Player
 call mvn clean package spring-boot:repackage -DskipTests
 cd ..
+cd angular-frontend
+call ng build --configuration production
+cd ..
 call docker-compose up
